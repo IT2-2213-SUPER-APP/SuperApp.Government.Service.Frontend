daa40512b124a494a564cbfd0782a40342b76299
dev2pew <ermekgd2004@gmail.com>
2025-10-24 18:28:25 +0500

    remove all

--- comments/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Comment
-
-@admin.register(Comment)
-class CommentAdmin(admin.ModelAdmin):
-    list_display = ('author', 'submission', 'content', 'created_at', 'parent')
-    list_filter = ('created_at',)
-    search_fields = ('content', 'author__username', 'submission__title')
--- scripts/migrate.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-poetry run python manage.py makemigrations
-poetry run python manage.py migrate
